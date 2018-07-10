554c3255af0a6de68fc00323948f547642419b0e
Darlan Cavalcante Moreira <darcamo@gmail.com>
2018-07-10 11:55:07 -0300

    Update build.py to only add one configuration
    The library is header-only and thus multiple configurations are unnecessary.

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="darcamo", channel="stable")
-    builder.add_common_builds()
+    builder.add()  # Header-only library
     builder.run()
